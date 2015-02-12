0ea5c6f817c6b58e66eaa5435bd53d64bd235305
Mingardi Stefano <mingardi@gmail.com>
2015-02-12 16:25:10 +0100

    Some Experiments with formats and route

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -13,5 +14,44 @@ class DefaultController extends Controller
     public function indexAction()
     {
         return $this->render('default/index.html.twig');
+    }
+
+    /**
+     * @Route("/app/rawresponse", name="raw_response")
+     */
+    public function rawAction()
+    {
+        return new Response('This is a raw response from inside controller');
+    }
+
+    /**
+     * @Route("/app/hello/{name}", name="hello")
+     */
+    public function helloAction($name)
+    {
+        return $this->render('default/hello.html.twig', array(
+            'name' => $name
+        ));
+    }
+
+    /**
+     * @Route("/app/json", defaults={"_format"="json"}, name="json")
+     */
+    public function jsonAction()
+    {
+        $test = array("format"=>"json");
+        return new Response(json_encode($test));
+    }
+
+    /**
+     * @Route("/app/format.{_format}",
+     *          defaults={"_format"="html"},
+     *          requirements = { "_format" = "html|xml|json" },
+     *          name="format"
+     * )
+     */
+    public function formatAction($_format)
+    {
+        return $this->render('default/format.'.$_format.'.twig');
     }
 }
